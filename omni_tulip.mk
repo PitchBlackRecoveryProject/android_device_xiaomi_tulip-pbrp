7b94642290159d5ab7e3715d6a080b8622f2fdb7
dianlujitao <dianlujitao@lineageos.org>
2019-03-23 16:17:36 +0800

    tulip: Fix HW FDE when building with Pie tree
     * ro.board.platform is no longer part of recovery ramdisk on Pie, thus
       hw_get_module_by_class would fail to find the keystore module and HW
       FDE is broken.
     * To address the issue, use a property to specify the HAL class to make
       the HAL picked by libhardware.
    
    Change-Id: I2b09a06be34ca66a400e98fce8714226ae5f3d10
    Signed-off-by: Albert I <kras@raphielgang.org>

--- omni_tulip.mk
+++ omni_tulip.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/pb/config/common.mk)
 # Props.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.treble.enabled=true \
+    ro.hardware.keystore=sdm660 \
     sys.usb.controller=a800000.dwc3 \
     sys.usb.rndis.func.name=rndis_bam \
     sys.usb.rmnet.func.name=rmnet_bam
